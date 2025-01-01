ea881c2e957669f15ccc2e088eeb851a83013504
B022110114 <b02110114@student.utem.edu.my>
2025-01-01 20:32:47 +0800

    port updated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();  // Add this line at the top
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const jwt = require('jsonwebtoken');
 
